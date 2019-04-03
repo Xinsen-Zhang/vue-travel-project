4358274fedff3a05be3bb84ebb67fa751983f992
xinsen <13051885033@163.com>
2019-04-04 00:15:59 +0800

    轮播图的完成

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,11 +2,8 @@
   <div class="wrapper">
     <swiper :options="swiperOption">
       <!-- slides -->
-      <swiper-slide>
-        <img src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/cd159d07551d6506de88e0e2b04047ce.jpg_750x200_8a1f27c0.jpg" alt="banner1" class="swiper-img">
-      </swiper-slide>
-      <swiper-slide>
-        <img src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/6b6fd3d9e40b749ac1d189ce80ebb3cc.jpg_750x200_9616e2e7.jpg" alt="banner2" class="swiper-img">
+      <swiper-slide v-for="item of imgList" :key="item.id">
+        <img :src="item.imgUrl" :alt="item.alt" class="swiper-img">
       </swiper-slide>
       <!-- Optional controls -->
       <div class="swiper-pagination"  slot="pagination"></div>
@@ -24,8 +21,21 @@ export default {
   data () {
     return {
       swiperOption: {
-        pagination: '.swiper-pagination'
-      }
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      imgList: [
+        {
+          id: '001',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/cd159d07551d6506de88e0e2b04047ce.jpg_750x200_8a1f27c0.jpg',
+          alt: '1号轮播图'
+        },
+        {
+          id: '002',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/6b6fd3d9e40b749ac1d189ce80ebb3cc.jpg_750x200_9616e2e7.jpg',
+          alt: '2号轮播图'
+        }
+      ]
     }
   }
 }
