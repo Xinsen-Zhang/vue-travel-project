63725b2110b91d07ba77bcf2b8955b836c1b40b0
xinsen <13051885033@163.com>
2019-04-03 22:22:34 +0800

    scale设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +8,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: resolve => require(['@/pages/home/Home.vue'], resolve)
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: resolve => require(['@/pages/list/List.vue'], resolve)
     }
   ]
 })
