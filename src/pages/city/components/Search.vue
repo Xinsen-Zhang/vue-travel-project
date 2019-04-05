6eb5d24d093db5a2d207c51b73d48f97c7b82fe4
wings0571 <13051885033@163.com>
2019-04-05 15:34:59 +0800

    搜索组件的输入框完成

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="search">
+    <div class="search-input">
+    <input type="text" placeholder="输入城市/拼音">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scope>
+@import '~styles/variables.styl'
+
+.search
+  background: $bgColor
+  width: 100%
+  position: relative
+  height  .88rem
+  .search-input
+    position: absolute
+    left 0
+    right 0
+    height .68rem
+    width 100%
+    box-sizing border-box
+    padding .10rem .80rem
+    background-color $bgColor
+    color #666
+    input
+      background #fff
+      width 100%
+      text-align center
+      box-sizing border-box
+      padding 0 0.2rem
+</style>
